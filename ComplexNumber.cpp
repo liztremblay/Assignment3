826400b8df88ad796350274f0b6782dd58d79f42
Ali Mekouar <medalimekouar2005@gmail.com>
2025-03-16 12:26:05 -0400

    op

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -10,3 +10,20 @@
 
 
 ComplexNumber::ComplexNumber(double r, double i) : real(r), imag(i) {}
+
+// (a + bi) + (c + di) = (a + c) + (b + d)i
+ComplexNumber ComplexNumber::operator + (const ComplexNumber& other) const {
+    return ComplexNumber(real + other.real, imag + other.imag);
+}
+
+// (a + bi) - (c + di) = (a - c) + (b - d)i
+ComplexNumber ComplexNumber::operator - (const ComplexNumber& other) const {
+    return ComplexNumber(real - other.real, imag - other.imag);
+}
+
+// (a + bi)(c + di) = ac + adi + bci + bdi^2 = (ac - bd) + (ad + bc)i
+ComplexNumber ComplexNumber::operator*(const ComplexNumber& other) const {
+    return ComplexNumber((real * other.real) - (imag * other.imag), (real * other.imag) + (imag * other.real))
+}
+
+// (a + bi) / (c + di) = [(a + bi)(c - di)] / (c^2 + d^2) = [(ac + bd) + (bc - ad)i] / (c^2 + d^2)
